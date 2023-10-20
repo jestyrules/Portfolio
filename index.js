e069ad83c3af63d51ed375e2538d8ada92ecb4a1
Jestin G Johnson <134034950+jestyrules@users.noreply.github.com>
2023-10-20 21:16:29 +0530

    Mail

---
+++ index.js
@@ -0,0 +1,25 @@
+function sendMail(){
+    var params ={
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        subject:document.getElementById("subject").value,
+        message:document.getElementById("message").value,
+    };
+
+
+const serviceID = "service_2cw5quk";
+const templateID = "template_17aizof"
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res=>{
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+    console.log(res);
+    alert("Your Message Sent Successfully");
+    }
+)
+.catch((err)=>console.log(err));
+}
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,34 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-   
+// Check for empty fields and valid email
+if (
+    empty($_POST['name']) ||
+    empty($_POST['email']) ||
+    empty($_POST['phone']) ||
+    empty($_POST['message']) ||
+    !filter_var($_POST['gmail'], FILTER_VALIDATE_EMAIL)
+) {
+    echo "No arguments Provided!";
+    exit; // Use exit instead of return to terminate the script
+}
+
+// Sanitize and store form data
 $name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$email_address = strip_tags(htmlspecialchars($_POST['gmail']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'jestinjohnson1@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+
+// Create the email content
+$to = 'jestinjohnson1@gmail.com'; // Replace with your email address
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+$email_body = "You have received a new message from your website contact form.\n\n"
+    . "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@gmail.com\n"; // Replace with a suitable 'from' address.
+$headers .= "Reply-To: $email_address";
+
+// Send the email
+if (mail($to, $email_subject, $email_body, $headers)) {
+    echo "Email sent successfully";
+} else {
+    echo "Email could not be sent. Check your server's mail configuration.";
+}
 ?>
